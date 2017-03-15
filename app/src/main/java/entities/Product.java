5c6f12c9d5f64dbfd28663b393ed22d3cf02ded2
Laurens @UIZ <laurens.decock@student.odisee.be>
2017-03-15 16:18:05 +0100

    Product class

---
+++ app/src/main/java/entities/Product.java
@@ -0,0 +1,44 @@
+package entities;
+
+/**
+ * Created by uizen on 15.03.2017.
+ */
+
+public class Product {
+    private String name;
+    private float price;
+    private int id;
+    private String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
