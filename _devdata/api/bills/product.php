c4b0d924df872c4dfc44ce1fb2c3b5aaeb1ff4d7
Laurens @UIZ <laurens.decock@student.odisee.be>
2017-05-02 09:56:10 +0200

    Add/delete product from bill in same endpoint with PUT & DELETE requests

---
+++ _devdata/api/bills/product.php
@@ -0,0 +1,97 @@
+<?php
+require("../config.php");
+require("../functions.php");
+
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD']; // HTTP Request method
+$input = file_get_contents("php://input"); // HTTP Request body (raw)
+
+if(!$input) {
+    error('No data provided');
+}
+
+if($method == 'PUT') {
+    $json = json_decode($input, true);
+
+    // Object checks
+    if(!isset($json['bill_id'])) {
+        error('Missing bill_id');
+    }
+
+    if(!isset($json['product_id'])) {
+        error('Missing product_id');
+    }
+
+    $bill_id = $json['bill_id'];
+    $product_id = $json['product_id'];
+
+    try {
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, $opt);
+
+        $stmt = $conn->prepare("INSERT INTO app_bill_has_products (bill_id, product_id) VALUES (:bill_id, :product_id)");
+        $stmt->bindParam(':bill_id', $bill_id);
+        $stmt->bindParam(':product_id', $product_id);
+        $stmt->execute();
+        $rowcount = $stmt->rowCount();
+        $success = ($rowcount > 0) ? true : false;
+        $returnObj = array('success' => $success);
+
+        echo json_encode($returnObj);
+        $conn = null;
+        die();
+    }
+    catch(PDOException $e) {
+        error($e->getMessage());
+    }
+}
+else if($method == 'DELETE') {
+
+    $json = json_decode($input, true);
+
+    // Object checks
+    if(!isset($json['bill_id'])) {
+        error('Missing bill_id');
+    }
+
+    if(!isset($json['product_id'])) {
+        error('Missing product_id');
+    }
+
+    $bill_id = $json['bill_id'];
+    $product_id = $json['product_id'];
+
+    try {
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, $opt);
+
+        $stmt = $conn->prepare("DELETE FROM app_bill_has_products WHERE bill_id = :bill_id AND product_id = :product_id");
+        $stmt->bindParam(':bill_id', $bill_id);
+        $stmt->bindParam(':product_id', $product_id);
+        $stmt->execute();
+        $rowcount = $stmt->rowCount();
+        $success = ($rowcount > 0) ? true : false;
+        $returnObj = array('success' => $success);
+
+        echo json_encode($returnObj);
+        $conn = null;
+        die();
+    }
+    catch(PDOException $e) {
+        error($e->getMessage());
+    }
+}
+else {
+    error('This route accepts PUT and DELETE requests only');
+}
