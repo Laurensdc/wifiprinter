0628433026a2b0925b8684dc8b1abf5442c80d98
Juliette Le Moal <juliette.le-moal@laposte.net>
2017-05-03 10:45:47 +0200

    merge printUI to develop

---
+++ app/src/main/java/helpers/OverviewAdapter.java
@@ -0,0 +1,72 @@
+package helpers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.httptandooripalace.restaurantorderprinter.R;
+import entities.Bill;
+import entities.Product;
+
+/**
+ * Created by uiz on 27/04/2017.
+ */
+
+public class OverviewAdapter extends BaseAdapter{
+
+    private Context context;
+    private List<Bill> bills;
+
+    public OverviewAdapter(Context c, List<Bill> bills) {
+        context = c;
+        this.bills = new ArrayList<>();
+        this.bills = bills;
+
+    }
+
+    @Override
+    public int getCount() {
+        if(bills==null){return 0;}
+        return bills.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return bills.get(position);
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        final Bill bill = bills.get(position);
+
+        if(convertView==null){
+            LayoutInflater inflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.overview_list_item, null);
+        }
+
+        TextView t1 = (TextView) convertView.findViewById(R.id.table_number);
+        t1.setText(bill.getTableNr());
+
+        TextView t2 = (TextView) convertView.findViewById(R.id.waiter_name);
+        t2.setText(bill.getWaiter());
+        return convertView;
+    }
+
+
+
+}
